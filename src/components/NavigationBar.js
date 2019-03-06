8201a2bc72e18d24e07ed0010cebfa436a392a3a
knd <khanhdao.knd@gmail.com>
2019-03-06 09:33:47 +0700

    Structure routes

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,4 @@
+export const NavigationBar = props => {
+  // TODO: Implement this
+  return null
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = props => {
+  // TODO: Implement here
+  return null
+}
+
+export default Home
